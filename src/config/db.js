a9264bc86571a2ccb2f0e0a25cd87f8970159b60
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-06-04 11:20:48 +0100

    Remove console.log

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,6 @@ const { getSecret } = require("../lib/secrets-manager")
 const getConfig = async () => {
   const secret = await getSecret(process.env.POSTGRES_SECRET_ID, true)
 
-  console.log(secret.host)
-
   const config = {
     host: secret.host,
     port: 5432,
